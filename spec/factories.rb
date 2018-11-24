afffcd27eb6734cd2715db1d3da5ea1c60a920c7
Jordan Perkic <jmaperkic@yahoo.com>
2018-11-24 20:49:48 +0000

    show page

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     password "secretPassword"
     password_confirmation "secretPassword"
   end
+
+  factory :gram do
+  	message "hello"
+  	association :user
+  end
+
+
+
 end
