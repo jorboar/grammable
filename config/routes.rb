db5805c846fda13cbba7f85109c53f3d3b774b98
Jordan Perkic <jmaperkic@yahoo.com>
2018-11-24 23:10:46 +0000

    destroy grams

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :models
 	root "grams#index"
-	resources :grams, only: [:new, :create, :show, :edit, :update]
+	resources :grams
 end
